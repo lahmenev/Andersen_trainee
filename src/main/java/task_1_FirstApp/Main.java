3e78190a9e52001618d4d47585daf14f5e429d4c
sergey <ser.lakhmenev1993@mail.ru>
2019-05-20 23:37:37 +0300

    Initial commit

---
+++ src/main/java/task_1_FirstApp/Main.java
@@ -0,0 +1,26 @@
+package task_1_FirstApp;
+
+/**
+ * email : s.lakhmenev@andersenlab.com
+ *
+ * @author Lakhmenev Sergey
+ * @version 1.1
+ */
+public class Main {
+    private static final String NAME = "Sergey";
+
+    /**
+     * Here start point of the program
+     * This method displays  text in console
+     * @param args command line   values
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, " + NAME + ". You've just started a very difficult way.\n" +
+                "You must choose your goals and what do you want to get in the end.\n" +
+                "\n" +
+                "Reminder:\n" +
+                "Rest for wimps.\n" +
+                "\n" +
+                "Sincerely Andersen");
+    }
+}
